42f1d0aa33ddaf22e2f871c76c4ff8e893241b9a
hjx <1585973539@qq.com>
2021-09-24 14:05:23 +0800

    在student类中增加了age，name属性，tostring方法，setget方法，添加了构造器

--- TsetModule01/src/com/mashibing/student.java
+++ TsetModule01/src/com/mashibing/student.java
@@ -5,4 +5,35 @@ package com.mashibing;
  * @create: 2021/9/24 13:59
  */
 public class student {
+    private int age;
+    private String Name;
+    
+    public student() {
+    }
+    
+    public student(int age, String name) {
+        this.age = age;
+        Name = name;
+    }
+    
+    public int getAge() {
+        return age;
+    }
+    
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+    public String getName() {
+        return Name;
+    }
+    
+    public void setName(String name) {
+        Name = name;
+    }
+    
+    @Override
+    public String toString() {
+        return "student{" + "age=" + age + ", Name='" + Name + '\'' + '}';
+    }
 }
